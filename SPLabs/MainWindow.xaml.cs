7d48476c0caba966c0531fce51beed7f4da9056a
Philipp Hamitsevich <spritefok@gmail.com>
2018-10-27 22:25:33 +0300

    Add Laboratory work 6

--- SPLabs/MainWindow.xaml.cs
+++ SPLabs/MainWindow.xaml.cs
@@ -9,20 +9,52 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SPLabs
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TextBlock text;
+        private ThicknessAnimation anim;
+        private bool active = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            if(text == null)
+            {
+                text = new TextBlock();
+                text.Text = "Lab 6 text";
+                text.FontSize = 24;
+                mainStack.Children.Add(text);
+            }
+            anim = new ThicknessAnimation();
+            anim.From = text.Margin;
+            anim.To = new Thickness(text.Margin.Left > 90?0:180, 0, 0, 0);
+            anim.Duration = TimeSpan.FromSeconds(2);
+            anim.AutoReverse = true;
+            anim.Completed += completed;
+            text.BeginAnimation(TextBlock.MarginProperty, anim);
+            active = true;
+
+        }
+        private void completed(object sender, EventArgs e)
+        {
+            if(active)
+                text.BeginAnimation(TextBlock.MarginProperty, anim);
+        }
+        private void Stop(object sender, RoutedEventArgs e) {
+            double ml = text.Margin.Left;
+            text.BeginAnimation(TextBlock.MarginProperty, null);
+            text.Margin = new Thickness(ml, 0, 0, 0);
+            active = false;
         }
     }
 }
