fd8e8f4a92fda046187cd7d78687985b27241072
Philipp Hamitsevich <spritefok@gmail.com>
2018-10-28 00:08:20 +0300

    Added Laboratory work 8

--- SPLab7/MainWindow.xaml.cs
+++ SPLab7/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using System.Windows.Shapes;
 
 namespace SPLab7
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public ObservableCollection<string> rItems = new ObservableCollection<string>();
