5c0c3c2986f43c86ab37d67635a0c63bd6cc72fd
Philipp Hamitsevich <spritefok@gmail.com>
2018-10-28 19:29:33 +0300

    Added Laboratory work 10

---
+++ SPLab10/DrawWin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SPLab10
+{
+    public partial class DrawWin : Window
+    {
+        private MainWindow win;
+        public DrawWin(MainWindow win)
+        {
+            InitializeComponent();
+            this.win = win;
+        }
+        private void Focus(object sender, EventArgs e)
+        {
+            MessageBox.Show("Focused");
+        }
+    }
+}
